86dd17f017e3aa999d1756ede36de48246907a76
dkoncius <as.koncius@gmail.com>
2023-02-16 18:40:56 +0200

    work

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import '@/styles/viewer.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Script from "next/script";
-import home from '@/styles/Home.module.css'
-
 import { useEffect, useRef, useState } from "react";
+import Router from 'next/router'
 
 export default function Home() {
   // 3D model
@@ -18,15 +17,32 @@ export default function Home() {
   
   function onModelChange(e) {
     setModel(...e.target.files)
-    alert("uploaded")
+    console.log(...e.target.files)
   }
 
   // Sizes
-  const modelSize = useRef(null)
+  const modelViewer = useRef(null)
   useEffect(() => {
-    modelSize.current.style.width = "100%"
-    modelSize.current.style.height = "90vh"
+    modelViewer.current.style.width = "100%"
+    modelViewer.current.style.height = "90vh"
   }, [])
+
+  // Load model
+  
+
+  // Slider data
+  const sliderData = ['Chair', 'Canoe', 'GeoPlanter', 'Mixer', "ToyTrain"]
+
+  // Change model
+  const switchSrc = (event, name) => {
+    modelViewer.src = `/slider/${name}.glb`;
+    // modelViewer.poster = `/glb/${name}.glb`;
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((element) => {element.classList.remove("selected");});
+    event.currentTarget.classList.add("selected")    
+  };
+  
+
 
 
   return (
@@ -36,13 +52,36 @@ export default function Home() {
         type="module" 
         src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.0.0/model-viewer.min.js"
       />
+
       {/* Upload model */}
       <input type="file" onChange={onModelChange} />
+
       {/* Model */}
-      <model-viewer ref={modelSize} src={modelUrl} ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1" render-scale="5">
+      <model-viewer ref={modelViewer} src={modelUrl} ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1" render-scale="5">
         <div class="progress-bar hide" slot="progress-bar">
             <div class="update-bar"></div>
         </div>
+
+      {/* Slider */}
+      <div className="slider">
+        <div className="slides">
+            {
+                sliderData.map(data => 
+                  <button 
+                  onClick={(e) => switchSrc(e, data)}
+                  className="slide"
+                  style={{
+                    background: `url(/slider/${data}.webp)`,
+                    backgroundSize: 'contain',
+                    backgroundColor: 'white',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat'
+                  }}
+                  ></button>
+                  )
+            }
+        </div>
+      </div>
       </model-viewer>
     
     </>
